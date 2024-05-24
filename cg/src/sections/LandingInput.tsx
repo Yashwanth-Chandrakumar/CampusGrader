96bc4a71b62987536d09826be9cb5f99cbdbbdb0
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-05-24 10:29:02 +0530

    Suggestion box added

--- cg/src/sections/LandingInput.tsx
+++ cg/src/sections/LandingInput.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { PlaceholdersAndVanishInput } from "../components/ui/placeholders-and-vanish-input";
+import Suggestions from "./Suggestions";
 
 export function PlaceholdersAndVanishInputDemo() {
   const placeholders = [
@@ -7,8 +9,9 @@ export function PlaceholdersAndVanishInputDemo() {
     "Why are you still waiting?",
     "Let's help one to have his future",
   ];
-
+  const [input, setInput] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
     console.log(e.target.value);
   };
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -16,12 +19,13 @@ export function PlaceholdersAndVanishInputDemo() {
     console.log("submitted");
   };
   return (
-    <div className="h-[5rem] flex flex-col justify-center  items-center px-4">
+    <div className=" flex flex-col justify-center  items-center px-4">
       <PlaceholdersAndVanishInput
         placeholders={placeholders}
         onChange={handleChange}
         onSubmit={onSubmit}
       />
+      <Suggestions inputValue={input} />
     </div>
   );
 }
