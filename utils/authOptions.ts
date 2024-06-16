6d6f163006a24ecc7449417afb0282e91b8c5a79
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-06-16 21:59:32 +0530

    build fix 1

---
+++ utils/authOptions.ts
@@ -0,0 +1,78 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/userSchema";
+import bcrypt from "bcryptjs";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordsMatch) {
+            throw new Error("Incorrect password");
+          }
+
+          return user;
+        } catch (error) {
+          console.log("Error manual login: ", error);
+          throw error;
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      console.log("User: ", user);
+      console.log("Account: ", account);
+      const { name, email } = user;
+      if (account.provider === "google") {
+        try {
+          const res = await fetch("http://localhost:3000/api/google", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name,
+              email,
+            }),
+          });
+          if (res.ok) {
+            return true;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      return true;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+
