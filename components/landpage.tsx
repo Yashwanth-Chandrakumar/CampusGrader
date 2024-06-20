ef7a0641f645fb348e136bc4450b24e3d229b5ca
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-06-20 18:14:50 +0530

    Update landpage.tsx

--- components/landpage.tsx
+++ components/landpage.tsx
@@ -23,7 +23,7 @@ export default function AuroraBackgroundDemo() {
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
           Maybe, his life too.
         </div>
-        <PlaceholdersAndVanishInputDemo navUrl="/auth/login"/>
+        <PlaceholdersAndVanishInputDemo/>
         {/* <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
           Debug now
         </button> */}
