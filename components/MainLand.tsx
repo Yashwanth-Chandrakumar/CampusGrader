3cfa89c783133ca4b7cf5b71c8f8fb8715f34ed7
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-06-21 15:27:19 +0530

    View page commit

--- components/MainLand.tsx
+++ components/MainLand.tsx
@@ -78,8 +78,8 @@ const Landingpage = () => {
               <div key={index} className="flex justify-between items-center text-gray-800 dark:text-gray-200 p-2 ">
                 <span>{college}</span>
                 <div className="flex space-x-2">
-                  <button type="button" onClick={()=>{router.push(`/view/${college}`)}} className="bg-gradient-to-br relative px-10 group/btn mt-2 from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">View</button>
-                  <button type="button" onClick={()=>{router.push(`/rate/${college}`)}} className="bg-gradient-to-br relative px-10 group/btn mt-2 from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">Rate</button>
+                  <button type="button" onClick={()=>{router.push(`/view/${encodeURIComponent(college)}`)}} className="bg-gradient-to-br relative px-10 group/btn mt-2 from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">View</button>
+                  <button type="button" onClick={()=>{router.push(`/rate/${encodeURIComponent(college)}`)}} className="bg-gradient-to-br relative px-10 group/btn mt-2 from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">Rate</button>
                 </div>
                 
               </div>
