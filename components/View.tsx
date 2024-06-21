ae761d565c749bb8b173c7db4271653e12abb582
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-06-21 17:21:27 +0530

    Updated rating component

--- components/View.tsx
+++ components/View.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import NavbarDemo from "./navbar";
 import Rating from "./rating/Rating";
 
 const View = ({college}:{college:string;}) => {
+
+    const [userRating, setUserRating] = useState(0);
+
   return (
     <div className="flex flex-col mt-32 text-black dark:text-white items-center min-h-screen py-2 bg-zinc-50 dark:bg-zinc-900 ">
         <NavbarDemo/>
         <div className="w-full max-w-7xl mt-10 p-10">
         <h1 className="text-2xl">{college}</h1>
-        <Rating isEditable={false} rating={4} />
+        <Rating isEditable={true} rating={userRating} setRating={setUserRating} />
+        <p>{userRating}</p>
         </div>
     </div>
   )
