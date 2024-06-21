0ebcf89e2fc07e09675d8ca89e09aa0cf9ec3148
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-06-21 21:35:54 +0530

    Admin fn for college

--- app/api/college/route.ts
+++ app/api/college/route.ts
@@ -1,13 +1,22 @@
-import { connectMongoDB } from '@/lib/mongodb'; // Adjust the path as needed
-import College from '@/models/collegeSchema'; // Adjust the path as needed
-import { ObjectId } from 'mongodb'; // Import ObjectId from mongodb
+import { connectMongoDB } from '@/lib/mongodb';
+import College from '@/models/collegeSchema';
+import { ObjectId } from 'mongodb';
 import { NextResponse } from 'next/server';
 
-export async function POST(req:any) {
+export async function POST(req: any) {
   try {
-    const { name, userId, academic, faculty, infrastructure, accommodation, socialLife, fee, placement, food } = await req.json();
+    const {
+      name, userId, 
+      academicRating, academicReview,
+      facultyRating, facultyReview,
+      infrastructureRating, infrastructureReview,
+      accommodationRating, accommodationReview,
+      socialLifeRating, socialLifeReview,
+      feeRating, feeReview,
+      placementRating, placementReview,
+      foodRating, foodReview
+    } = await req.json();
 
-    // Validate and convert userId to ObjectId
     if (!ObjectId.isValid(userId)) {
       return NextResponse.json({ message: 'Invalid userId format' }, { status: 400 });
     }
@@ -17,19 +26,19 @@ export async function POST(req:any) {
     const newCollege = await College.create({
       name,
       userId: objectId,
-      academic,
-      faculty,
-      infrastructure,
-      accommodation,
-      socialLife,
-      fee,
-      placement,
-      food
+      academicRating, academicReview,
+      facultyRating, facultyReview,
+      infrastructureRating, infrastructureReview,
+      accommodationRating, accommodationReview,
+      socialLifeRating, socialLifeReview,
+      feeRating, feeReview,
+      placementRating, placementReview,
+      foodRating, foodReview
     });
 
     return NextResponse.json(newCollege, { status: 201 });
   } catch (error) {
-    console.error('Error creating college:', error); // Enhanced error logging
+    console.error('Error creating college:', error);
     return NextResponse.json(
       { message: 'Error creating college', error: error },
       { status: 500 }
