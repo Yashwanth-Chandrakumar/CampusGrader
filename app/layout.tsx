7481fc099da7d1570375c39412cb9d85ed02d88e
Yashwanth S C <yashwanth2k05@gmail.com>
2024-07-15 19:33:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
       <head>
       <meta name="google-adsense-account" content="ca-pub-1517362125342301"></meta>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1517362125342301"crossOrigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1517362125342301" crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>
         {children}
